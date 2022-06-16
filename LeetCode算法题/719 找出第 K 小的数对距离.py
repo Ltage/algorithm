34773da2421f4053472a294510ec7b5d52a227f5
Ltage <18016065002@163.com>
2022-06-16 12:40:49 +0800

    update

--- LeetCode算法题/719 找出第 K 小的数对距离.py
+++ LeetCode算法题/719 找出第 K 小的数对距离.py
@@ -11,13 +11,17 @@ class Solution:
         right = nums[-1] - nums[0]
         while left <= right:
             mid = (right - left) // 2 + left
-            idx = len(nums) - 1
             cnt = 0
-            while idx > 0:
-                rank = bisect.bisect_left(nums, nums[idx] - mid)
-                if rank < idx:
-                    cnt += idx - rank
-                idx -= 1
+            l = 0
+            r = 0
+            while l < len(nums) - 1:
+                if r < len(nums):
+                    while nums[r] - nums[l] <= mid:
+                        r += 1
+                        if r > len(nums) - 1:
+                            break
+                cnt += r - l - 1
+                l += 1
             if cnt < k:
                 left = mid + 1
             else:
@@ -26,4 +30,5 @@ class Solution:
         return left
 
 
-print(Solution.smallestDistancePair([1, 7, 10, 15, 15], 3))
+print(Solution.smallestDistancePair([62, 100, 4],
+                                    2))
